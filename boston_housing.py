bed9e8d00c314cec5930c54bf86754b7e18dfcb8
jmhorn <john.mi.horn@gmail.com>
2018-11-17 15:48:07 -0800

    convnet

---
+++ boston_housing.py
@@ -0,0 +1,28 @@
+from keras.datasets import boston_housing
+from keras import models
+from keras import layers
+import numpy as np
+
+(train_data, train_targets), (test_data, test_targets) = boston_housing.load_data()
+
+mean = train_data.mean(axis=0)
+train_data -= mean
+std = train_data.std(axis=0)
+train_data /= std
+
+test_data -= mean
+test_data /= std
+
+def build_model():
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation='relu', input_shape=(train_data.shape[1],)))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(1))
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+return model
+
+k = 4
+num_val_samples = len(train_data) // k
+num_epochs = 100
+all_scores = []
+swza
--- reueters.py
+++ reueters.py
@@ -25,6 +25,7 @@ one_hot_test_labels = to_categorical(test_labels)
 model = models.Sequential()
 model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(46, activation='softmax'))
 
 # Model Compiling
